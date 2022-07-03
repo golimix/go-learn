1fe53c7ecaafd88d3f8c4d3b7b9b19615d92b37f
limix <coollimix@gmail.com>
2022-07-03 10:01:08 +0800

    添加指针类型spce和样例

--- spec/syntax/typing/pointer_test.go
+++ spec/syntax/typing/pointer_test.go
@@ -1,9 +1,19 @@
 package typing
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestTypingPointerSpec(t *testing.T) {
 	println(`
 		PointerType = "*" BaseType .
 		BaseType    = Type .`)
+}
+
+func TestTypingPointerSample(t *testing.T) {
+	var a *emptyStruct = &emptyStruct{}
+	var b *[4]int
+	println(reflect.TypeOf(a).String())
+	println(reflect.TypeOf(b).String())
 }
