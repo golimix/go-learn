72b4ababb7282c1a2a32b25a6658798bca2d300c
limix <coollimix@gmail.com>
2022-07-03 09:45:56 +0800

    数组测试逻辑更新

--- spec/syntax/typing/array_test.go
+++ spec/syntax/typing/array_test.go
@@ -25,15 +25,16 @@ func TestTypingArraySpec(t *testing.T) {
 	`)
 }
 
-func TestTypingArray(t *testing.T) {
-	println(`
-		ArrayType   = "[" ArrayLength "]" ElementType .
-		ArrayLength = Expression .
-		ElementType = Type .`)
+func TestTypingArraySample(t *testing.T) {
 	arr := [3]string{"hello", "cool", "limix"}
 	fmt.Printf("%s, 长度:%d, 容量:%d 类型:%s\n", arr, len(arr), cap(arr), reflect.TypeOf(arr).String())
 	arr = [...]string{"hello", "cool", "limix"}
 	fmt.Printf("%s, 长度:%d, 容量:%d 类型:%s\n", arr, len(arr), cap(arr), reflect.TypeOf(arr).String())
 
+	// 数据下标访问时,不能超出其范围
+	// arr[3] = "limix"
+	fmt.Printf("%s 数组下标访问越界\n", `arr[3]="limix"`)
+
 	// TODO 向数据类型中添加一个元素
+
 }
