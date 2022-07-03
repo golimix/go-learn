296b70e66bc4aa2e89e49576a635fca3ba018b1e
limix <coollimix@gmail.com>
2022-07-03 11:39:38 +0800

    更新预定义标识符

--- spec/lexical/identifer/identifier_test.go
+++ spec/lexical/identifer/identifier_test.go
@@ -11,9 +11,22 @@ func TestIdentifier(t *testing.T) {
 
 // 预定义标识符
 func TestIdentifierPredeclared(t *testing.T) {
-	// 预定义类型标识符
 	fmt.Println("预定义标识符")
-	fmt.Println("=========================================")
-	fmt.Println("any comparable\nbool byte complex64 complex128 error float32 float64\n int int8 int16 int32 int64 rune string\n uint uint8 uint16 uint32 uint64 uintptr")
-	fmt.Println("=========================================")
+	fmt.Println(`
+		Types:
+			any bool byte comparable
+			complex64 complex128 error float32 float64
+			int int8 int16 int32 int64 rune string
+			uint uint8 uint16 uint32 uint64 uintptr
+
+		Constants:
+			true false iota
+
+		Zero value:
+			nil
+
+		Functions:
+			append cap close complex copy delete imag len
+			make new panic print println real recover	
+	`)
 }
